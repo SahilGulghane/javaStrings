28ac37c8c34be1792de7ebf9113b96787d19ea88
SahilGulghane <101868182+SahilGulghane@users.noreply.github.com>
2022-09-13 11:37:03 -0700

    length of string

---
+++ src/com/company/Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+
+// note use each code sepratly...
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//to copy string.....
+//step 1 convert given string into array....
+//step 2 make an empty char array of size == length oof given string....
+//step 3 copy conetent of array till size == lentgh.....
+
+//        String st = "radar";
+//        char y[] = st.toCharArray();
+//        int size = st.length();
+//        char z[] = new char[size];
+//        int i = 0;
+//        while (i != size){
+//            z[i] = y[i];
+//            i++;
+//
+//        }
+//        System.out.println(z);
+
+
+
+//to make reverse string...
+//step 1 same as above step 1 and step 2....
+//step 2 last data of 1st array to 1st data off 2nd array till ist array pointer reaches to size-1 to 0....
+
+//        String st = "sahil";
+//        char y[] = st.toCharArray();
+//        int size = st.length();
+//        char z[] = new char[size];
+//        int i = 0;
+//        int j ;
+//        for(j = size-1;j>=0 ;j--){
+//            z[i] = y[j];
+//
+//
+//            i++;
+//        } System.out.println(z);
+
+
+
+//to check string as palindrome(after reversing also string reamins same civic radar racecar)
+//        String st = "sahil";
+//        char y[] = st.toCharArray();
+//        int size = st.length();
+//        char z[] = new char[size];
+//        int i = 0;
+//        int j ;
+//        for(j = size-1;j>=0 ;j--){
+//            z[i] = y[j];
+//
+//
+//            i++;
+//        }
+//        int k ;
+//        for (k = 0 ; k<st.length();k++ ){
+//            if(y[k] != z[k]){
+//                System.out.println("not palindrome");
+//                System.exit(0);
+//
+//            }
+//            else {
+//                continue;
+//            }
+//
+//        }
+//        System.out.println("palindrome");
+
+
+
+
+
+
+
+
+    }
+}
