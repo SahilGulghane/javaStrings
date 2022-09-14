05a2fc851e16710b5fec0f5cae2d1567f25d6f80
SahilGulghane <101868182+SahilGulghane@users.noreply.github.com>
2022-09-14 07:49:23 -0700

    upperlowercase

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,57 +1,134 @@
 package com.company;
 
-
-// note use each code sepratly...
+import  java.util.Scanner;
+// following function will convert string into lower upper upperincludespace upper1stalphabet
 
 
 import java.util.Locale;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-
-//to check string as palindrome(after reversing also string remains same civic radar racecar)\
-//        but there is limitation such as blank spaces in between as well as come upper case and lower case character...
-//        how to overcome....
-//        step1 replace() is used to replace data of string at once so blank spaces are replaced by no spaces...
-//        step 2 either convert complete string in uppercase or lowercase using
-//                toLowerCase() and to toUppercase() methode.....
-        String st = "too hot to Hoot";
-        st = st.replace(" ","");
-
-        st = st.toLowerCase();
+class Operation {
+    //this function will convert provided string into lowercase without giving blank spaces
+    public void onlylower() {
+        Scanner sc = new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter a string: ");
+        String st = sc.nextLine();
+        st = st.replace(" ", "");
         char y[] = st.toCharArray();
         int size = st.length();
-        char z[] = new char[size];
         int i = 0;
-        int j ;
-        for(j = size-1;j>=0 ;j--){
-            z[i] = y[j];
-
-
-            i++;
-        }
-        int k ;
-        for (k = 0 ; k<st.length();k++ ){
-            if(y[k] != z[k]){
-                System.out.println("not palindrome");
-                System.exit(0);
-
-            }
-            else {
-                continue;
+        while (i != size) {
+            if (y[i] >= 65 && y[i] <= 90) {
+                y[i] = (char) (y[i] + 32);
+                i++;
+            } else if (y[i] >= 97 && y[i] <= 122) {
+                y[i] = y[i];
+                i++;
             }
 
+
         }
-        System.out.println("palindrome");
+        System.out.println("entered string: " + st);
+        System.out.println(y);
+
+    }
+
+// this function will convert string into uppercase removing blank spaces
+    public void onlyupper() {
+        Scanner sc = new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter a string: ");
+        String st = sc.nextLine();
+        st = st.replace(" ", "");
+        char y[] = st.toCharArray();
+        int size = st.length();
+        int i = 0;
+        while (i != size) {
+            if (y[i] >= 65 && y[i] <= 90) {
+                y[i] = y[i];
+                i++;
+            } else if (y[i] >= 97 && y[i] <= 122) {
+
+                y[i] = (char) (y[i] - 32);
+                i++;
+            }
+
+
+        }
+        System.out.println("entered string: " + st);
+        System.out.println(y);
+
+    }
+//this function will covert string into uppercase considering blank spaces
+    public void onlyupperwithoutreplace() {
+        Scanner sc = new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter a string: ");
+        String st = sc.nextLine();
+
+        char y[] = st.toCharArray();
+        int size = st.length();
+        int i = 0;
+        while (i != size) {
+            if (y[i] >= 65 && y[i] <= 90) {
+                y[i] = y[i];
+                i++;
+            } else if (y[i] >= 97 && y[i] <= 122) {
+
+                y[i] = (char) (y[i] - 32);
+                i++;
+            }
+            else if(y[i] == 32){
+                y[i] = y[i];
+                i++;
+            }
+
+
+        }
+        System.out.println("entered string: " + st);
+        System.out.println(y);
+
+    }
+
+// this function will convert 1st alphabet of word in to upper case...
+    public void onlyupper1staplhabet() {
+        Scanner sc = new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Enter a string: ");
+        String st = sc.nextLine();
+
+        char y[] = st.toCharArray();
+        int size = st.length();
+        int i = 0;
+        while (i != size) {
+                if(y[i] == 32 ) {
+                   y[i+1] = (char)(y[i+1] - 32);
+
+                }i++;
 
 
 
-
-
-
-
+        }
+        System.out.println("entered string: " + st);
+        System.out.println(y);
 
     }
 }
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Operation st = new Operation();
+        st.onlylower();
+        st.onlyupper();
+        st.onlyupperwithoutreplace();
+        st.onlyupper1staplhabet();
+
+    }
+}
+
+
+
+
+
+
+
