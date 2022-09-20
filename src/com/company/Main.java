6e609c3c481996a1e1b824609728da74436115ad
SahilGulghane <101868182+SahilGulghane@users.noreply.github.com>
2022-09-20 12:09:07 -0700

    to delete the character from an string

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
 // to check weather string is rotational copy or not
 // concat() to attach the given part
 // contains() return bool means is a part
     public static void main(String[] args) {
-        String currents = "aradhya";
-        String key = "dhyaara";
-        String ns ;
-        ns =  currents.concat(currents);
-        if(ns.contains(key)){
-            System.out.println(ns.contains(key));
-            System.out.println("rotational String  ");
+
+        Scanner sc =new Scanner(System.in);
+        System.out.println("enter the string");
+        String currents = sc.nextLine();
+
+        char y[] =  currents.toCharArray();
+        System.out.println("enter the character you want to delete from string");
+        char key = sc.next().charAt(0);
+        int i =0;
+        String ns = "";
+        while (i != y.length){
+
+            if(y[i] != key){
+                ns = ns+y[i];
+            }i++;
         }
-        else
-        {
-            System.out.println("non rotational string");
-        }
+        System.out.println(ns);
 
     }
 }
