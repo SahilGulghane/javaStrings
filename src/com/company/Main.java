39f14ad21930a2b2d08a995c256996f470be24bb
SahilGulghane <101868182+SahilGulghane@users.noreply.github.com>
2022-09-23 08:52:57 -0700

    n palindrome check

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,70 @@
 package com.company;
 
+
+// note use each code sepratly...
+
+
+import java.util.Locale;
 import java.util.Scanner;
+class Check{
+    public void ch(){
+
+              Scanner sc = new Scanner(System.in);
+              String st = sc.nextLine();
+                st = st.toLowerCase();
+                char y[] = st.toCharArray();
+                int size = st.length();
+                char z[] = new char[size];
+                int i = 0;
+                int j ;
+                for(j = size-1;j>=0 ;j--){
+                    z[i] = y[j];
+
+
+                    i++;
+                }
+                int k ;
+                for (k = 0 ; k<st.length();k++ ){
+                    if(y[k] != z[k]){
+                        System.out.println("not palindrome");
+                        return;
+
+                    }
+                    else {
+                        continue;
+                    }
+
+                }
+                System.out.println("palindrome");
+
+
+
+
+
+
+
+    }
+
+}
 
 public class Main {
 
-
     public static void main(String[] args) {
 
-        String s ="@hello world my name is sahil ";
+
+
+        Check one = new Check();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int i =0;
-        int count =0;
-        char y[] = s.toCharArray();
-        while(i != s.length()){
-            if(y[i] != ' '){
-                if(y[i] >= 'a' && y[i] <= 'z'){
-                    count++;
-                    i++;
-
-
-                }
-                else{
-                    i++;
-                }
-
-              }
-            else{
-                System.out.print(count+" ");
-
-                i++;
-
-            }
+        while (i != n){
+            one.ch();
+            i++;
         }
+
+
+
+
 
     }
 }
