a9baee7b2e03e20faa4133c601a230aaa175183c
SahilGulghane <101868182+SahilGulghane@users.noreply.github.com>
2022-09-22 22:00:00 -0700

    to count alphabet in word also space after each whitespace

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,33 @@ import java.util.Scanner;
 
 public class Main {
 
-// to check weather string is rotational copy or not
-// concat() to attach the given part
-// contains() return bool means is a part
+
     public static void main(String[] args) {
 
-        Scanner sc =new Scanner(System.in);
-        System.out.println("enter the string");
-        String currents = sc.nextLine();
-        char y[] =  currents.toCharArray();
-        System.out.println("enter the targeted character");
-        char target = sc.next().charAt(0);
-        System.out.println("enter the letter which want to be replace");
-        char key = sc.next().charAt(0);
-
+        String s ="@hello world my name is sahil ";
         int i =0;
-        while (i != currents.length()){
-            if(y[i] == target){
-                y[i] =key;
-            }i++;
+        int count =0;
+        char y[] = s.toCharArray();
+        while(i != s.length()){
+            if(y[i] != ' '){
+                if(y[i] >= 'a' && y[i] <= 'z'){
+                    count++;
+                    i++;
+
+
+                }
+                else{
+                    i++;
+                }
+
+              }
+            else{
+                System.out.print(count+" ");
+
+                i++;
+
+            }
         }
-        System.out.println(y);
 
     }
 }
